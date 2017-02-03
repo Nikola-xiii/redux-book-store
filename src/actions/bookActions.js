bbe0cd9dd5dbbe792208f31b5c33b002981b2451
Nikola Serdiuk <nikola.serdiuk@gmail.com>
2017-02-03 16:40:43 +0200

    Action with Redux Create

---
+++ src/actions/bookActions.js
@@ -0,0 +1,6 @@
+export const CreateBook = (book) => {
+  return {
+    type: 'CREATE_BOOK',
+    book
+  };
+};
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
+import configureStore from './store/configureStore';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
+const store = configureStore();
+
 render(
-  <Router routes={routes} history={browserHistory} />,
+  <Provider store={store}>
+    <Router routes={routes} history={browserHistory} />
+  </Provider>,
   document.getElementById('app')
 );
